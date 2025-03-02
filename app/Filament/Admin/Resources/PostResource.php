d9ea0886599ed9252aea9aa0dd60f29130a4a78c
anishregmi17 <anishregminaglibang@gmail.com>
2025-03-02 17:12:43 +0545

    feat: add Post resource with CRUD functionality and migration

---
+++ app/Filament/Admin/Resources/PostResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\PostResource\Pages;
+use App\Filament\Admin\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Dom\Text;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextInputColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->placeholder('Enter the title of the post')
+                    ->rules('required', 'string', 'max:255'),
+                TextInput::make('content')
+                    ->required()
+                    ->placeholder('Enter the content of the post')
+                    ->rules('required', 'string'),
+            ]);
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextInputColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                TextInputColumn::make('content')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'view' => Pages\ViewPost::route('/{record}'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
